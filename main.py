919754d9d7e114968d1ad538243dfbd7e6c3b49a
brian <briankathukya2000@gmail.com>
2020-07-26 13:46:50 +0300

    video pixel operations

--- main.py
+++ main.py
@@ -13,19 +13,56 @@ def video():
     if request.method == "POST":
         f = request.files['file']
         filename = f.filename
-        if filename != '':
-            file_ext = os.path.splitext(filename)[1]
-            if file_ext not in app.config['VIDEO_EXTENSIONS']:
-                flash('File format not supported',category='danger')
-                return redirect(url_for('video'))
-            else:
-                try:
-                    cmd = 'ffmpeg -i $HOME/Downloads/Video/{} $HOME/Downloads/Music/{}'.format(filename,os.path.splitext(filename)[0]+".mp3")
-                    subprocess.call(cmd,shell=True)
-                    flash("Video conversion completed successfully",category="success")
-                except:
-                    flash('The video could not be converted, try again!')
+        if f:
+            if filename != '':
+                file_ext = os.path.splitext(filename)[1]
+                
+                if file_ext not in app.config['VIDEO_EXTENSIONS']:
+                    flash('File format not supported',category='danger')
                     return redirect(url_for('video'))
+                else:
+                    if 'tomp3' in request.form:
+                        try:
+                            cmd = 'ffmpeg -i $HOME/Downloads/Video/{} $HOME/Downloads/Music/{}'.format(filename,os.path.splitext(filename)[0]+".mp3")
+                            subprocess.run(cmd,shell=True)
+                            flash("Video conversion completed successfully",category="success")
+                        except:
+                            flash('The video could not be converted, try again!',category='danger')
+                            return redirect(url_for('video'))
+                    elif 'to720' in request.form:
+                        try:
+                            cmd = 'ffmpeg -y -i $HOME/Downloads/Video/{} -c:a aac -ac 2 -ab 128k -c:v libx264 -x264opts keyint=24:min-keyint=24:no-scenecut -b:v 1500k -maxrate 1500k -bufsize 1500k -vf scale=720:-2 $HOME/Downloads/Video/{}'.format(filename,os.path.splitext(filename)[0]+"720"+os.path.splitext(filename)[1])
+                            subprocess.run(cmd,shell=True)
+                            flash("Video was converted to 720p", category="success")
+                        except:
+                            flash("The conversion could not be completed, try again!",category="danger")
+                            return redirect(url_for('video'))
+                    elif 'to540' in request.form:
+                        try:
+                            cmd = 'ffmpeg -y -i $HOME/Downloads/Video/{} -c:a aac -ac 2 -ab 128k -c:v libx264 -x264opts keyint=24:min-keyint=24:no-scenecut -b:v 1500k -maxrate 1500k -bufsize 1500k -vf scale=540:-2 $HOME/Downloads/Video/{}'.format(filename,os.path.splitext(filename)[0]+"540"+os.path.splitext(filename)[1])
+                            subprocess.run(cmd,shell=True)
+                            flash("Video was converted to 540p", category="success")
+                        except:
+                            flash("The conversion could not be completed, try again!",category="danger")
+                            return redirect(url_for('video'))
+                    elif 'to360' in request.form:
+                        try:
+                            cmd = 'ffmpeg -y -i $HOME/Downloads/Video/{} -c:a aac -ac 2 -ab 128k -c:v libx264 -x264opts keyint=24:min-keyint=24:no-scenecut -b:v 1500k -maxrate 1500k -bufsize 1500k -vf scale=360:-2 $HOME/Downloads/Video/{}'.format(filename,os.path.splitext(filename)[0]+"360"+os.path.splitext(filename)[1]) 
+                            subprocess.run(cmd,shell=True)
+                            flash("Video was successfully converted to 360p",category="success")
+                        except:
+                            flash('The conversion could not complete',category="danger")
+                            return redirect(url_for('video'))
+                    elif 'to144' in request.form:
+                        try:
+                            cmd = 'ffmpeg -y -i $HOME/Downloads/Video/{} -c:a aac -ac 2 -ab 128k -c:v libx264 -x264opts keyint=24:min-keyint=24:no-scenecut -b:v 1500k -maxrate 1500k -bufsize 1500k -vf scale=144:-2 $HOME/Downloads/Video/{}'.format(filename,os.path.splitext(filename)[0]+"144"+os.path.splitext(filename)[1]) 
+                            subprocess.run(cmd,shell=True)
+                            flash("Video was successfully converted to 144p",category="success")
+                        except:
+                            flash('The conversion could not complete',category="danger")
+                            return redirect(url_for('video'))
+        else:           
+            flash('No file was provided',category='danger')
     return render_template('video.html')
 
 @app.route('/audio')
